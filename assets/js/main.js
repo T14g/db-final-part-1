1e62390dfd14c563f7dff4933828b4e1d2f9be6c
T14g <T14g>
2021-01-18 19:20:22 -0300

    Js format

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 //Input Masks
 $('#cpf').inputmask('999.999.999-99');
 $('#celular').inputmask('(99) 99999-9999');
-$('#cep').inputmask('99999-999', {"placeholder": ""});
+$('#cep').inputmask('99999-999', { "placeholder": "" });
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('#send-form').click(function(e){
+    $('#send-form').click(function (e) {
         e.preventDefault();
 
-        if(runValidations()){
+        if (runValidations()) {
             toastr.success("Formulário validado com sucesso!");
             showSuccessMsg();
         }
@@ -16,11 +16,11 @@ $(document).ready(function(){
     });
 
     //Event Handler ao digitar um CEP
-    $('#cep').keyup(function(){
+    $('#cep').keyup(function () {
 
-        var cepFormated = $(this).val().replace('-','');
+        var cepFormated = $(this).val().replace('-', '');
 
-        if(cepFormated.length === 8){
+        if (cepFormated.length === 8) {
             getCepData(cepFormated);
         }
 
@@ -30,25 +30,25 @@ $(document).ready(function(){
 });
 
 //Busca informações relativas ao CEP
-function getCepData(cep){
+function getCepData(cep) {
 
     var url = 'https://api.postmon.com.br/v1/cep/' + cep;
     console.log("Pesquisando cep");
 
     axios.get(url)
-        .then(function(response) {
+        .then(function (response) {
 
             var data = response.data;
             updateAddress(data);
 
-    })
-        .catch(function(error) {
+        })
+        .catch(function (error) {
             console.log(error)
-    })
+        })
 
 }
 
-//Atualiza informações de logradouro, cidade, estado
+//Atualiza informações de logradouro, cidade, estado e bairro
 function updateAddress(data) {
 
     $('#logradouro').val(data.logradouro);
@@ -62,62 +62,62 @@ function updateAddress(data) {
 function runValidations() {
     var validated = true;
 
-    if(!validateName()){
+    if (!validateName()) {
         validated = false;
         toastr.error('Nome inválido');
     }
 
-    if(!validateDate()){
+    if (!validateDate()) {
         validated = false;
         toastr.error('Data inválida');
     }
 
-    if(!validateCPF()){
+    if (!validateCPF()) {
         validated = false;
         toastr.error('CPF inválido');
     }
 
-    if(!validateEmail()){
+    if (!validateEmail()) {
         validated = false;
         toastr.error('Email inválido');
     }
-    
-    if(!validateCellphone()){
+
+    if (!validateCellphone()) {
         validated = false;
         toastr.error('Celular inválido');
     }
 
-    if(!validateCEP()){
+    if (!validateCEP()) {
         validated = false;
         toastr.error('CEP inválido');
     }
 
-    if(!validateLogradouro()){
+    if (!validateLogradouro()) {
         validated = false;
         toastr.error('Logradouro inválido');
     }
 
-    if(!validateNumero()){
+    if (!validateNumero()) {
         validated = false;
         toastr.error('Número da casa/apto inválido');
     }
 
-    if(!validateCity()){
+    if (!validateCity()) {
         validated = false;
         toastr.error('Cidade inválida');
     }
 
-    if(!validateBairro()){
+    if (!validateBairro()) {
         validated = false;
         toastr.error('Bairro inválido');
     }
 
-    if(!validateUF()){
+    if (!validateUF()) {
         validated = false;
         toastr.error('UF inválida');
     }
 
-    if(!validateBio()){
+    if (!validateBio()) {
         validated = false;
         toastr.error('Bio inválida');
     }
@@ -129,14 +129,14 @@ function runValidations() {
 //Exibe dados no console
 function showSuccessMsg() {
 
-    var dateParts =  $('#nascimento').val().split('-');
+    var dateParts = $('#nascimento').val().split('-');
     var formatedDate = '';
     formatedDate = dateParts[2] + '-' + dateParts[1] + '-' + dateParts[0];
 
     console.log("Formulário validado! Confira os dados abaixo:");
     console.log("Nome completo: " + $('#nome-completo').val());
     console.log("Data de nascimento: " + formatedDate);
-    console.log("CPF: " + $('#cpf').val()); 
+    console.log("CPF: " + $('#cpf').val());
     console.log("Celular: " + $('#celular').val());
     console.log("Email: " + $('#email').val());
     console.log("CEP: " + $('#cep').val());
@@ -150,26 +150,26 @@ function showSuccessMsg() {
 }
 
 //Valida o nome que precisa ter pelo menos 3 letras
-function validateName(){
+function validateName() {
 
     var nome = $('#nome-completo').val();
-    
-    if(nome.length < 3){
+
+    if (nome.length < 3) {
         return false
-    }else{
+    } else {
         return true;
     }
 
 }
 
 //Valida a  data de nascimento
-function validateDate(){
+function validateDate() {
 
     var nascimento = $('#nascimento').val();
 
-    if(nascimento){
+    if (nascimento) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
@@ -181,9 +181,9 @@ function validateCPF() {
     var cpf = $('#cpf').val();
     var regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
 
-    if(regex.test(cpf)){
+    if (regex.test(cpf)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
@@ -192,12 +192,12 @@ function validateCPF() {
 //Valida o email
 function validateEmail() {
 
-    var email       = $('#email').val();
+    var email = $('#email').val();
     var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-    if(regex.test(email.toLowerCase())){
+
+    if (regex.test(email.toLowerCase())) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
@@ -206,24 +206,24 @@ function validateEmail() {
 //Valida o celular
 function validateCellphone() {
 
-    var celular     = $('#celular').val();
+    var celular = $('#celular').val();
     var regex = /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/;
 
-    if(regex.test(celular)){
+    if (regex.test(celular)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 //Valida CEP
-function validateCEP(){
+function validateCEP() {
 
     var cep = $('#cep').val();
 
-    if(cep.length >= 8){
+    if (cep.length >= 8) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
@@ -231,77 +231,77 @@ function validateCEP(){
 
 
 //Valida Logradouro
-function validateLogradouro(){
+function validateLogradouro() {
 
-    var logradouro  = $('#logradouro').val();
+    var logradouro = $('#logradouro').val();
 
-    if(logradouro.length > 0){
+    if (logradouro.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
 }
 
 //Valida número da casa/prédio
-function validateNumero(){
+function validateNumero() {
 
     var numero = $('#numero').val();
 
-    if(numero.length > 0){
+    if (numero.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 //Valida cidade
-function validateCity(){
+function validateCity() {
 
     var city = $('#cidade').val();
 
-    if(city.length > 0){
+    if (city.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
 }
 
 //Valida bairro
-function validateBairro(){
+function validateBairro() {
 
-    var bairro      = $('#bairro').val();
+    var bairro = $('#bairro').val();
 
-    if(bairro.length > 0){
+    if (bairro.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
 }
 
 //Valida UF
-function validateUF(){
+function validateUF() {
 
     var uf = $('#uf').val();
 
-    if(uf.length > 0){
+    if (uf.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
 }
 
 //Valida Bio
-function validateBio(){
+function validateBio() {
 
     var bio = $('#bio').val();
 
-    if(bio.length > 0){
+    if (bio.length > 0) {
         return true;
-    }else{
+    } else {
         return false;
     }
 
